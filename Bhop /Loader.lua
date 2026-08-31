0f515fca142cd0c95270487b86e03d089216ca18
sbrick98 <sbrick981234@gmail.com>
2026-09-01 01:38:52 +0900

    Create Loader.lua

---
+++ Bhop /Loader.lua
@@ -0,0 +1,5 @@
+-----/Basic/-----
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sbrick981234-prog/Pulse-s-Scripts/refs/heads/main/Bhop%20/Source.lua"))()
+
+-----/Modded/-----
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sbrick981234-prog/Pulse-s-Scripts/refs/heads/main/Bhop%20/Modded.lua"))()
