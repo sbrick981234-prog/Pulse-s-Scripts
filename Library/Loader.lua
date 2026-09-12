b6809b22dd0e52566b9336ace9f85bbf1c47694a
sbrick98 <sbrick981234@gmail.com>
2026-09-12 13:39:30 +0900

    Create Loader.lua

---
+++ Library/Loader.lua
@@ -0,0 +1,5 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/sbrick981234-prog/Pulse-s-Scripts/refs/heads/main/Library/Source.lua"))()
+
+Library:Drag(MainFrame)
+Library:Cells(MainFrame)
+Library:Stars(MainFrame)
