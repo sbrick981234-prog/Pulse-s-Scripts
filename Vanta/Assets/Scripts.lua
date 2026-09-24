843bc3824669b0a52e93f8c949845ff314fd9f5e
sbrick98 <sbrick981234@gmail.com>
2026-09-25 01:29:30 +0900

    Create Scripts.lua

---
+++ Vanta/Assets/Scripts.lua
@@ -0,0 +1,14 @@
+local ScriptList = {
+	{
+		Name = "Fling Deluxe",
+		Source = [[
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/sbrick981234-prog/Pulse-s-Scripts/refs/heads/main/Infinity%20Yield%20Fling/Source.lua"))()
+		]]
+	},
+	{
+		Name = "Salute",
+		Source = [[
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/sbrick981234-prog/Pulse-s-Scripts/refs/heads/main/Salute/Source.lua"))()
+		]]
+	}
+}
